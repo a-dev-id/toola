3003ae00fd6efdc45ad97af5ed790890f252084e
A-Dev <3xasov@gmail.com>
2023-10-24 20:21:57 +0800

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,117 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities." />
+    <meta name="keywords" content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app" />
+    <meta name="author" content="pixelstrap" />
+    <link rel="icon" href="{{asset('vendors/viho')}}/assets/images/favicon.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{asset('vendors/viho')}}/assets/images/favicon.png" type="image/x-icon" />
+    <title>viho - Premium Admin Template</title>
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet" />
+    <!-- Font Awesome-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/fontawesome.css" />
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/icofont.css" />
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/themify.css" />
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/flag-icon.css" />
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/feather-icon.css" />
+    <!-- Plugins css start-->
+    <!-- Plugins css Ends-->
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/bootstrap.css" />
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/style.css" />
+    <link id="color" rel="stylesheet" href="{{asset('vendors/viho')}}/assets/css/color-1.css" media="screen" />
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/responsive.css" />
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<body>
+    <!-- Loader starts-->
+    <div class="loader-wrapper">
+        <div class="theme-loader">
+            <div class="loader-p"></div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    </div>
+    <!-- Loader ends-->
+    <!-- page-wrapper Start-->
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-7">
+                    <img class="bg-img-cover bg-center" src="{{asset('vendors/viho')}}/assets/images/login/2.jpg" alt="looginpage" />
+                </div>
+                <div class="col-xl-5 p-0">
+                    <div class="login-card">
+                        <form class="theme-form login-form" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <h4>Login</h4>
+                            <h6>Welcome back! Log in to your account.</h6>
+                            <div class="form-group">
+                                <label>Email Address</label>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="icon-email"></i></span>
+                                    <input class="form-control" type="email" required placeholder="john@doe.com" name="email" value="{{old('email')}}" />
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="icon-lock"></i></span>
+                                    <input class="form-control" type="password" name="password" required placeholder="password" />
+                                    <div class="show-hide">
+                                        <span class="show"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox">
+                                    <input id="checkbox1" type="checkbox" name="remember" />
+                                    <label class="text-muted" for="checkbox1">Remember password</label>
+                                </div>
+                                <a class="link" href="forget-password.html">Forgot password?</a>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary btn-block" type="submit">
+                                    Sign in
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+    <!-- page-wrapper end-->
+    <!-- latest jquery-->
+    <script src="{{asset('vendors/viho')}}/assets/js/jquery-3.5.1.min.js"></script>
+    <!-- feather icon js-->
+    <script src="{{asset('vendors/viho')}}/assets/js/icons/feather-icon/feather.min.js"></script>
+    <script src="{{asset('vendors/viho')}}/assets/js/icons/feather-icon/feather-icon.js"></script>
+    <!-- Sidebar jquery-->
+    <script src="{{asset('vendors/viho')}}/assets/js/sidebar-menu.js"></script>
+    <script src="{{asset('vendors/viho')}}/assets/js/config.js"></script>
+    <!-- Bootstrap js-->
+    <script src="{{asset('vendors/viho')}}/assets/js/bootstrap/popper.min.js"></script>
+    <script src="{{asset('vendors/viho')}}/assets/js/bootstrap/bootstrap.min.js"></script>
+    <!-- Plugins JS start-->
+    <!-- Plugins JS Ends-->
+    <!-- Theme js-->
+    <script src="{{asset('vendors/viho')}}/assets/js/script.js"></script>
+    <!-- login js-->
+    <!-- Plugin used-->
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+</body>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,228 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="{{asset('vendors/viho')}}/assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('vendors/viho')}}/assets/images/favicon.png" type="image/x-icon">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <!-- Font Awesome-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/fontawesome.css">
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/icofont.css">
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/themify.css">
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/flag-icon.css">
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/feather-icon.css">
+    <!-- Plugins css start-->
+    <!-- Plugins css Ends-->
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/bootstrap.css">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/style.css">
+    <link id="color" rel="stylesheet" href="{{asset('vendors/viho')}}/assets/css/color-1.css" media="screen">
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendors/viho')}}/assets/css/responsive.css">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    @vite(['resources/js/app.js'])
+    @livewireStyles
+</head>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+<body>
+    <!-- Loader starts-->
+    <div class="loader-wrapper">
+        <div class="theme-loader">
+            <div class="loader-p"></div>
         </div>
-    </body>
+    </div>
+    <!-- Loader ends-->
+    <!-- page-wrapper Start-->
+    <div class="page-wrapper" id="pageWrapper">
+        <!-- Page Header Start-->
+        <div class="page-main-header">
+            <div class="main-header-right row m-0">
+                <div class="main-header-left">
+                    <div class="logo-wrapper"><a href="index.html"><img class="img-fluid" src="{{asset('vendors/viho')}}/assets/images/logo/logo.png" alt=""></a></div>
+                    <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle"></i></div>
+                </div>
+                <div class="nav-right col pull-right right-menu p-0">
+                    <ul class="nav-menus">
+                        <li class="onhover-dropdown">
+                            <div class="notification-box"><i data-feather="bell"></i><span class="dot-animated"></span></div>
+                            <ul class="notification-dropdown onhover-show-div">
+                                <li>
+                                    <p class="f-w-700 mb-0">You have 3 Notifications<span class="pull-right badge badge-primary badge-pill">4</span></p>
+                                </li>
+                                <li class="noti-primary">
+                                    <div class="media"><span class="notification-bg bg-light-primary"><i data-feather="activity"> </i></span>
+                                        <div class="media-body">
+                                            <p>Delivery processing </p><span>10 minutes ago</span>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="noti-secondary">
+                                    <div class="media"><span class="notification-bg bg-light-secondary"><i data-feather="check-circle"> </i></span>
+                                        <div class="media-body">
+                                            <p>Order Complete</p><span>1 hour ago</span>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="noti-success">
+                                    <div class="media"><span class="notification-bg bg-light-success"><i data-feather="file-text"> </i></span>
+                                        <div class="media-body">
+                                            <p>Tickets Generated</p><span>3 hour ago</span>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="noti-danger">
+                                    <div class="media"><span class="notification-bg bg-light-danger"><i data-feather="user-check"> </i></span>
+                                        <div class="media-body">
+                                            <p>Delivery Complete</p><span>6 hour ago</span>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="onhover-dropdown p-0">
+                            <button class="btn btn-primary-light" type="button"><a href="login_two.html"><i data-feather="log-out"></i>Log out</a></button>
+                        </li>
+                    </ul>
+                </div>
+                <div class="d-lg-none mobile-toggle pull-right w-auto"><i data-feather="more-horizontal"></i></div>
+            </div>
+        </div>
+        <!-- Page Header Ends-->
+        <!-- Page Body Start-->
+        <div class="page-body-wrapper horizontal-menu">
+            <!-- Page Sidebar Start-->
+            <header class="main-nav">
+                <div class="sidebar-user text-center">
+                    <img class="img-90 rounded-circle" src="{{asset('vendors/viho')}}/assets/images/dashboard/1.png" alt="">
+                    <a href="user-profile.html">
+                        <h6 class="mt-3 f-14 f-w-600">Emay Walter</h6>
+                    </a>
+                </div>
+                <nav>
+                    <div class="main-navbar">
+                        <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
+                        <div id="mainnav">
+                            <ul class="nav-menu custom-scrollbar">
+                                <li class="back-btn">
+                                    <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
+                                </li>
+                                <li class="sidebar-main-title">
+                                    <div>
+                                        <h6>General </h6>
+                                    </div>
+                                </li>
+                                <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('dashboard')}}"><i data-feather="home"></i><span>Dashboard</span></a></li>
+
+                            </ul>
+                        </div>
+                        <div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
+                    </div>
+                </nav>
+            </header>
+            <!-- Page Sidebar Ends-->
+            <div class="page-body">
+                <div class="container-fluid">
+                    <div class="page-header">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h3>Sample Page</h3>
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                    <li class="breadcrumb-item">Pages</li>
+                                    <li class="breadcrumb-item active">Sample Page</li>
+                                </ol>
+                            </div>
+                            <div class="col-sm-6">
+                                <!-- Bookmark Start-->
+                                <div class="bookmark">
+                                    <ul>
+                                        <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top" title="" data-original-title="Tables"><i data-feather="inbox"></i></a></li>
+                                        <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top" title="" data-original-title="Chat"><i data-feather="message-square"></i></a></li>
+                                        <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top" title="" data-original-title="Icons"><i data-feather="command"></i></a></li>
+                                        <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top" title="" data-original-title="Learning"><i data-feather="layers"></i></a></li>
+                                        <li><a href="javascript:void(0)"><i class="bookmark-search" data-feather="star"></i></a>
+                                            <form class="form-inline search-form">
+                                                <div class="form-group form-control-search">
+                                                    <input type="text" placeholder="Search..">
+                                                </div>
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- Bookmark Ends-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Container-fluid starts-->
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-header pb-0">
+                                    <h5>Sample Card</h5><span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                                </div>
+                                <div class="card-body">
+                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Container-fluid Ends-->
+            </div>
+            <!-- footer start-->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6 footer-copyright">
+                            <p class="mb-0">Copyright 2021-22 © viho All rights reserved.</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="pull-right mb-0">Hand crafted & made with <i class="fa fa-heart font-secondary"></i></p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <!-- latest jquery-->
+    <script src="{{asset('vendors/viho')}}/assets/js/jquery-3.5.1.min.js"></script>
+    <!-- feather icon js-->
+    <script src="{{asset('vendors/viho')}}/assets/js/icons/feather-icon/feather.min.js"></script>
+    <script src="{{asset('vendors/viho')}}/assets/js/icons/feather-icon/feather-icon.js"></script>
+    <!-- Sidebar jquery-->
+    <script src="{{asset('vendors/viho')}}/assets/js/sidebar-menu.js"></script>
+    <script src="{{asset('vendors/viho')}}/assets/js/config.js"></script>
+    <!-- Bootstrap js-->
+    <script src="{{asset('vendors/viho')}}/assets/js/bootstrap/popper.min.js"></script>
+    <script src="{{asset('vendors/viho')}}/assets/js/bootstrap/bootstrap.min.js"></script>
+    <!-- Plugins JS start-->
+    <!-- Plugins JS Ends-->
+    <!-- Theme js-->
+    <script src="{{asset('vendors/viho')}}/assets/js/script.js"></script>
+    {{-- <script src="{{asset('vendors/viho')}}/assets/js/theme-customizer/customizer.js"></script> --}}
+    <!-- login js-->
+    <!-- Plugin used-->
+
+    @livewireScripts
+</body>
+
 </html>
